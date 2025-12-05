c8d5f9f046657784290a3602aa7f87e074163da7
ytg <522125842@qq.com>
2025-12-05 15:53:15 +0800

    Initial Commit

---
+++ key-display/key_displayApp.swift
@@ -0,0 +1,17 @@
+//
+//  key_displayApp.swift
+//  key-display
+//
+//  Created by ytg on 2025/12/5.
+//
+
+import SwiftUI
+
+@main
+struct key_displayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
